bd069775a4d8151d3ca940d6c1a55185bb30c50c
sachin_silva <sachin_s@epiclanka.net>
2024-09-08 10:48:54 +0530

    init

---
+++ src/main/java/com/example/awsassignment/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.example.awsassignment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @PostMapping("/login")
+    public String loginSubmit(@RequestParam("username") String username,
+                              @RequestParam("password") String password,
+                              Model model) {
+        if ("user".equals(username) && "password".equals(password)) {
+            return "redirect:/home";
+        } else {
+            model.addAttribute("error", "Invalid username or password");
+            return "login";
+        }
+    }
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
